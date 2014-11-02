ac83648eb01a1d0d24ccdb4d89d0eaac23dc9ef0
hailxl <hailxl@gmail.com>
2014-11-02 16:17:26 +0800

    exclude the *.new file when rename files

--- init_project.sh
+++ init_project.sh
@@ -7,10 +7,12 @@ given_name=$1
 grep -r pyempty . | awk -F':' '{print $1}' | sort -u | grep -v Binary | grep -v init_project.sh | xargs sed -i .new "s/pyempty/$given_name/g"
 
 #rename file include default project name to the given project name
-for foo in $(find . -type f -name '*pyempty*');do
+for foo in $(find . -type f -name '*pyempty*' | grep -v '\.new');do
     echo "mv $foo $(echo $foo | sed "s/pyempty/$given_name/g")"
+    mv $foo $(echo $foo | sed "s/pyempty/$given_name/g")
 done
 
-for foo in $(find . -type d -name '*pyempty*');do
+for foo in $(find . -type d -name '*pyempty*' | grep -v '\.new');do
     echo "mv $foo $(echo $foo | sed "s/pyempty/$given_name/g")"
+    mv $foo $(echo $foo | sed "s/pyempty/$given_name/g")
 done
