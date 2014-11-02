ba1240338e07c68b78ceba0748daf119cf769d92
hailxl <hailxl@gmail.com>
2014-11-02 16:08:18 +0800

    script for init project files to your real project

---
+++ init_project.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+given_name=$1
+[[ -z "$given_name" ]] && echo "Usage: $0 new_project_name" && exit 1
+
+#replace all default project name to the given project name
+grep -r pyempty . | awk -F':' '{print $1}' | sort -u | grep -v Binary | grep -v init_project.sh | xargs sed -i .new "s/pyempty/$given_name/g"
+
+#rename file include default project name to the given project name
+for foo in $(find . -type f -name '*pyempty*');do
+    echo "mv $foo $(echo $foo | sed "s/pyempty/$given_name/g")"
+done
+
+for foo in $(find . -type d -name '*pyempty*');do
+    echo "mv $foo $(echo $foo | sed "s/pyempty/$given_name/g")"
+done
